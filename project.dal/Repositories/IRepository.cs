81c1db834def17d0c9f8b0416f6994ec64c3824d
Adam Malysák <xmalys02@vutbr.cz>
2023-04-03 16:45:46 +0200

    Repository_interface

---
+++ project.dal/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project.DAL.Entities;
+
+namespace project.DAL.Repositories
+{
+    public interface IRepository<TEntity>
+        where TEntity : class, IEntityID
+    {
+        IQueryable<TEntity> Get();
+        void Delete(Guid entityId);
+        ValueTask<bool> ExistsAsync(TEntity entity);
+        Task<TEntity> InsertAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
