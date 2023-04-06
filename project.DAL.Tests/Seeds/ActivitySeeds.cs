012fe6f83955f75f434c8b81bb0cbb77aca90a9e
Ondřej Koumar <xkouma02@vutbr.cz>
2023-04-07 00:21:58 +0200

    feat: "Seeding data" for activites

---
+++ project.DAL.Tests/Seeds/ActivitySeeds.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Metrics;
+using project.DAL.Entities;
+
+namespace project.DAL.Tests.Seeds;
+
+public static class ActivitySeeds
+{
+    private static int _counter = 0;
+    private static int ActivityCounter() => ++_counter; 
+    public static ActivityEntity ActivitySeed()
+    {
+        return new ActivityEntity
+        {
+            Id = Guid.NewGuid(),
+            DateTimeFrom = default,
+            DateTimeTo = default,
+            Name = $"random activity number {ActivityCounter()}",
+            Color = 0,
+            Project = null,
+            ProjectId = null,
+            User = null,
+            UserId = default
+        };
+
+    }
+}
