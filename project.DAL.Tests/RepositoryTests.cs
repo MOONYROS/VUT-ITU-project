14b1b7f50a0b3ec372006596efbab180d22a8c53
Pavol Šumichrast <xsumic01@vutbr.cz>
2023-04-08 14:34:24 -0700

    new: tests added need to be fixed

--- project.DAL.Tests/RepositoryTests.cs
+++ project.DAL.Tests/RepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using project.DAL.Entities;
 using project.DAL.Mappers;
 using project.DAL.Repositories;
@@ -17,17 +18,83 @@ namespace project.DAL.Tests
         public async Task RepositoryGetTest()
         {
             var user = UserSeeds.UserSeed();
-
             ProjectDbContextSUT.Users.Add(user);
             await ProjectDbContextSUT.SaveChangesAsync();
 
             await using var dbx = await DbContextFactory.CreateDbContextAsync();
-            var dbEntity = await dbx.Users.SingleAsync(i => i.Id == user.Id);
-
             var Mapper = new UserEntityMapper();
             var Repo = new Repository<UserEntity>(dbx, Mapper);
-            var Cokoliv = Repo.Get();
-            var Result = Cokoliv.Where(i => i.Id == user.Id);
+
+            var dbFromRepo = Repo.Get();
+
+            var dbFromRepoList = dbFromRepo.ToList();
+            Assert.Equal(dbFromRepoList.Count, dbx.Users.Count());
+            Assert.Equal(dbFromRepoList.First().FullName, dbx.Users.First().FullName);
+            Assert.Equal(dbFromRepoList.First().UserName, dbx.Users.First().UserName);
+        }
+        [Fact]
+        public async Task RepositoryExistsTest()
+        {
+            var userOutOfDbx = UserSeeds.UserSeed();
+            var userInDbx = UserSeeds.UserSeed();
+            ProjectDbContextSUT.Users.Add(userInDbx);
+            await ProjectDbContextSUT.SaveChangesAsync();
+
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var Mapper = new UserEntityMapper();
+            var Repo = new Repository<UserEntity>(dbx, Mapper);
+
+            var trueCase = await Repo.ExistsAsync(userInDbx);
+            var falseCase = await Repo.ExistsAsync(userOutOfDbx);
+
+            Assert.True(trueCase);
+            Assert.False(falseCase);
+        }
+        [Fact]
+        public async Task RepositoryInsertTest()
+        {
+            var user = UserSeeds.UserSeed();
+
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var Mapper = new UserEntityMapper();
+            var Repo = new Repository<UserEntity>(dbx, Mapper);
+
+            await Repo.InsertAsync(user);
+
+            var dbxUserList = dbx.Users.ToList();
+            Assert.Equal(user.FullName, dbxUserList[0].FullName);
+            Assert.Equal(user.UserName, dbxUserList[0].UserName);
+        }
+        [Fact]
+        public async Task RepositoryUpdateTest()
+        {
+            var user = UserSeeds.UserSeed();
+            ProjectDbContextSUT.Users.Add(user);
+            await ProjectDbContextSUT.SaveChangesAsync();
+
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var Mapper = new UserEntityMapper();
+            var Repo = new Repository<UserEntity>(dbx, Mapper);
+
+            user.UserName = "Test";
+            await Repo.UpdateAsync(user);
+
+            Assert.Equal(user.UserName, dbx.Users.First().UserName);
+        }
+        [Fact]
+        public async Task RepositoryDeleteTest()
+        {
+            var user = UserSeeds.UserSeed();
+            ProjectDbContextSUT.Users.Add(user);
+            await ProjectDbContextSUT.SaveChangesAsync();
+
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var Mapper = new UserEntityMapper();
+            var Repo = new Repository<UserEntity>(dbx, Mapper);
+
+            Repo.Delete(user.Id);
+            var userList = dbx.Users.ToList();
+            Assert.True(userList.IsNullOrEmpty());
         }
     }
 }
