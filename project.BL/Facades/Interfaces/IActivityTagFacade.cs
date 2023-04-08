c24be6f0f8aeeb6c2bd8986851b1b01e7797fcb9
Ondřej Koumar <xkouma02@vutbr.cz>
2023-04-09 01:01:38 +0200

    fix: Added implementation(inheritance) of IFacade to IActivityTagFacade

--- project.BL/Facades/Interfaces/IActivityTagFacade.cs
+++ project.BL/Facades/Interfaces/IActivityTagFacade.cs
@@ -1,4 +1,7 @@
-﻿namespace project.BL.Facades.Interfaces;
-public interface IActivityTagFacade 
+﻿using project.BL.Models;
+using project.DAL.Entities;
+
+namespace project.BL.Facades.Interfaces;
+public interface IActivityTagFacade : IFacade<ActivityTagListEntity, ActivityTagListModel, ActivityTagDetailModel>
 {
 }
