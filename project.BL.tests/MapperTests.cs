a60865d9e3263be73a4d0ccedf8163c8e64d90fc
Ondřej Koumar <xkouma02@vutbr.cz>
2023-04-09 13:05:37 +0200

    tests: New mapper test
    First one from models to entity; will need more in the future

--- project.BL.tests/MapperTests.cs
+++ project.BL.tests/MapperTests.cs
@@ -3,6 +3,7 @@ using project.BL.Mappers;
 using project.DAL.Tests;
 using project.DAL.Tests.Seeds;
 using project.BL.Models;
+using project.DAL.Entities;
 
 namespace project.BL.tests;
 
@@ -59,7 +60,7 @@ public class MapperTests : DbContextTestsBase
         var mappedActivity = activityMapper.MapToDetailModel(activity);
         
         var activityTagListMapper = new ActivityTagModelMapper();
-        activityTagListMapper.AddTagToActivity(refTag, mappedActivity);
+        activityTagListMapper.AddTagToActivity_Models(refTag, mappedActivity);
         
         DeepAssert.Equal(refActivity, mappedActivity);
     }
@@ -80,6 +81,7 @@ public class MapperTests : DbContextTestsBase
     [Fact]
     public void MapProjectWithUsers_EntityToModel()
     {
+        // Entities
         var user1 = UserSeeds.UserSeed();
         var user2 = UserSeeds.UserSeed();
         var project = ProjectSeeds.ProjectSeed();
@@ -123,5 +125,99 @@ public class MapperTests : DbContextTestsBase
         DeepAssert.Equal(refProject, mappedProject);
         DeepAssert.Equal(refUser1, mappedUser1);
         DeepAssert.Equal(refUser2, mappedUser2);
+    }
+
+    [Fact]
+    public void MapActivityWithTags_ModelToEntity()
+    {
+        // Models
+        var user = ModelSeeds.UserSeeds.UserSeed();
+        var activity = ModelSeeds.ActivitySeeds.ActivityDetailSeed();
+        var tag1 = ModelSeeds.TagSeeds.TagDetailSeed();
+        var tag2 = ModelSeeds.TagSeeds.TagDetailSeed();
+        
+        var tag1InActivity = new ActivityTagDetailModel
+        {
+            Id = Guid.NewGuid(),
+            ActivityId = activity.Id,
+            TagId = tag1.Id
+        };
+        var tag2InActivity = new ActivityTagDetailModel
+        {
+            Id = Guid.NewGuid(),
+            ActivityId = activity.Id,
+            TagId = tag2.Id
+        };
+        activity.Tags.Add(tag1);
+        activity.Tags.Add(tag2);
+
+        // Mappers
+        var activityMapper = new ActivityModelMapper();
+        var tagMapper = new TagModelMapper();
+        var activityTagMapper = new ActivityTagModelMapper();
+
+        // Mapping to entities
+        var mappedActivity = activityMapper.MapToEntity(activity, user.Id, null);
+        var mappedTag1 = tagMapper.MapToEntity(tag1);
+        var mappedTag2 = tagMapper.MapToEntity(tag2);
+        var mappedTag1InActivity = activityTagMapper.MapToEntity(activity, tag1);
+        var mappedTag2InActivity = activityTagMapper.MapToEntity(activity, tag2);
+        
+        // Adding tags to activities
+        activityTagMapper.AddTagToActivity_Entities(mappedActivity, mappedTag1, mappedTag1InActivity);
+        activityTagMapper.AddTagToActivity_Entities(mappedActivity, mappedTag2, mappedTag2InActivity);
+        
+        // Reference activities and tags
+        var refActivity = new ActivityEntity
+        {
+            Id = activity.Id,
+            DateTimeFrom = activity.DateTimeFrom,
+            DateTimeTo = activity.DateTimeTo,
+            Name = activity.Name,
+            Description = activity.Description,
+            Color = activity.Color.ToArgb(),
+            Project = null,
+            ProjectId = null,
+            User = null,
+            UserId = user.Id
+        };
+        var refTag1InActivity = new ActivityTagListEntity
+        {
+            // The Id of reference tag has to be the same as mapped, but it doesn't affect the quality of tests at all
+            Id = mappedTag1InActivity.Id,
+            ActivityId = tag1InActivity.ActivityId,
+            Activity = null,
+            TagId = tag1InActivity.TagId,
+            Tag = null
+        };
+        var refTag2InActivity = new ActivityTagListEntity
+        {
+            Id = mappedTag2InActivity.Id,
+            ActivityId = tag2InActivity.ActivityId,
+            Activity = null,
+            TagId = tag2InActivity.TagId,
+            Tag = null
+        };
+        var refTag1 = new TagEntity
+        {
+            Id = tag1.Id,
+            Name = tag1.Name,
+            Color = tag1.Color.ToArgb(),
+        };
+        var refTag2 = new TagEntity
+        {
+            Id = tag2.Id,
+            Name = tag2.Name,
+            Color = tag2.Color.ToArgb(),
+        };
+        activityTagMapper.AddTagToActivity_Entities(refActivity, refTag1, refTag1InActivity);
+        activityTagMapper.AddTagToActivity_Entities(refActivity, refTag2, refTag2InActivity);
+
+        // Asserts
+        DeepAssert.Equal(refActivity, mappedActivity);
+        DeepAssert.Equal(refTag1, mappedTag1);
+        DeepAssert.Equal(refTag2, mappedTag2);
+        DeepAssert.Equal(refTag1InActivity, mappedTag1InActivity);
+        DeepAssert.Equal(refTag2InActivity, mappedTag2InActivity);
     }
 }
