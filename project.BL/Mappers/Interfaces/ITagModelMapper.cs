9135894ffc3c7878a2fe70b957656ca54dfe19e0
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-03 14:39:27 +0200

    feat: map activityTagList to tagDetailModel

--- project.BL/Mappers/Interfaces/ITagModelMapper.cs
+++ project.BL/Mappers/Interfaces/ITagModelMapper.cs
@@ -1,11 +1,19 @@
-﻿using project.BL.Models;
+﻿using System.Collections;
+using Microsoft.IdentityModel.Tokens;
+using project.BL.Models;
 using project.DAL.Entities;
 
 namespace project.BL.Mappers.Interfaces;
 
-public interface ITagModelMapper
+public interface ITagModelMapper : IModelMapperDetailOnly<TagEntity, TagDetailModel>
 {
     TagDetailModel MapToDetailModel(ActivityTagListEntity entity);
+
     IEnumerable<TagDetailModel> MapToDetailModel(IEnumerable<ActivityTagListEntity> entities)
-        => entities.Select(MapToDetailModel);
+    {
+        var activityTagListEntities = entities.ToList();
+        return activityTagListEntities.IsNullOrEmpty() ? 
+            Enumerable.Empty<TagDetailModel>() : 
+            activityTagListEntities.Select(MapToDetailModel);
+    }
 }
