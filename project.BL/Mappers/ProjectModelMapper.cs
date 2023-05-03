13939b257c702750ce2ff2b753a5474a875eefec
Patrik Nehéz <xnehez01@vutbr.cz>
2023-05-03 16:07:47 +0200

    Project mapper

--- project.BL/Mappers/ProjectModelMapper.cs
+++ project.BL/Mappers/ProjectModelMapper.cs
@@ -1,23 +1,42 @@
-﻿using project.BL.Mappers.Interfaces;
+﻿using System.Drawing;
+using project.BL.Mappers.Interfaces;
 using project.BL.Models;
 using project.DAL.Entities;
+using project.BL;
+using System.Diagnostics;
 
 namespace project.BL.Mappers;
 
 public class ProjectModelMapper : ModelMapperBase<ProjectEntity, ProjectListModel, ProjectDetailModel>
 {
+    private readonly IUserModelMapper _userModelMapper;
+    public ProjectModelMapper(IUserModelMapper userModelMapper)
+    {
+        _userModelMapper = userModelMapper;
+    }
     public override ProjectListModel MapToListModel(ProjectEntity? entity)
-    {
-        throw new NotImplementedException();
-    }
+        => entity is null ?
+        ProjectListModel.Empty :
+        new ProjectListModel
+        {
+            Name = entity.Name
+        };
 
-    public override ProjectEntity MapToEntity(ProjectDetailModel model)
+    public override ProjectEntity MapToEntity(ProjectDetailModel project)
+    => new()
     {
-        throw new NotImplementedException();
-    }
+        Id = project.Id,
+        Name = project.Name,
+        Description = project.Description
+    };
 
-    public override ProjectDetailModel MapToDetailModel(ProjectEntity entity)
-    {
-        throw new NotImplementedException();
-    }
+    public override ProjectDetailModel MapToDetailModel(ProjectEntity? entity)
+        => entity is null ?
+        ProjectDetailModel.Empty :
+        new ProjectDetailModel
+        {
+            Name = entity.Name,
+            Description = entity.Description,
+            Users = _userModelMapper.MapToListModel(entity.Users).ToObservableCollection()
+        };
 }
