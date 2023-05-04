58fd9d9fdc6d53e7b7fbd825d3120d3b445b5833
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-04 15:49:49 +0200

    fix: Remove unused facades

--- project.BL/Facades/Interfaces/IUserProjectFacade.cs
+++
@@ -1,8 +0,0 @@
-﻿using project.BL.Models;
-using project.DAL.Entities;
-
-namespace project.BL.Facades.Interfaces;
-
-public interface IUserProjectFacade : IFacade<UserProjectListEntity, UserProjectListModel, UserProjectDetailModel>
-{
-}
