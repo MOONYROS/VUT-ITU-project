8abefc330acfac50d7807d027db8fd2e350f68dd
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-04 15:54:29 +0200

    feat: detailOnly facade base

---
+++ project.BL/Facades/FacadeBaseDetailOnly.cs
@@ -0,0 +1,20 @@
+﻿using project.BL.Mappers.Interfaces;
+using project.BL.Models;
+using project.DAL.Entities;
+using project.DAL.Mappers;
+using project.DAL.UnitOfWork;
+
+namespace project.BL.Facades;
+
+public class FacadeBaseDetailOnly<TEntity, TDetailModel, TEntityMapper>
+    where TEntity : class, IEntityID
+    where TDetailModel : class, IModel
+    where TEntityMapper : IEntityIDMapper<TEntity>, new()
+{
+    protected FacadeBaseDetailOnly(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IModelMapperDetailOnly<TEntity, TDetailModel> modelMapper)
+    {
+        throw new NotImplementedException();
+    }
+}
