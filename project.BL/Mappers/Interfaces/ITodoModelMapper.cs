fb087e1e26e7d647360fa80e5c65c2cd22d5be78
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-04 15:54:11 +0200

    refactor: new mapper interfaces for code consistency

---
+++ project.BL/Mappers/Interfaces/ITodoModelMapper.cs
@@ -0,0 +1,8 @@
+﻿using project.BL.Models;
+using project.DAL.Entities;
+
+namespace project.BL.Mappers.Interfaces;
+
+public interface ITodoModelMapper : IModelMapperDetailOnly<TodoEntity, TodoDetailModel>
+{
+}
