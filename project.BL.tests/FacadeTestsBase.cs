532a2e2745a944566c449c84c186362e47c73107
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-06 23:11:58 +0200

    test: Add facade test base and simple user tests

---
+++ project.BL.tests/FacadeTestsBase.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
+using project.DAL.Tests;
+using project.DAL.UnitOfWork;
+using Xunit.Abstractions;
+using project.BL.Mappers;
+using project.DAL;
+using project.DAL.Mappers;
+
+namespace project.BL.tests;
+
+public class FacadeTestsBase : IAsyncLifetime
+{
+    protected FacadeTestsBase(ITestOutputHelper output)
+    {
+        TagModelMapper = new TagModelMapper();
+        TodoModelMapper = new TodoModelMapper();
+        UserModelMapper = new UserModelMapper();
+        ProjectModelMapper = new ProjectModelMapper();
+        ActivityModelMapper = new ActivityModelMapper();
+
+        ActivityEntityMapper = new ActivityEntityMapper();
+        ActivityTagListEntityMapper = new ActivityTagListEntityMapper();
+        ProjectEntityMapper = new ProjectEntityMapper();
+        TagEntityMapper = new TagEntityMapper();
+        TodoEntityMapper = new TodoEntityMapper();
+        UserEntityMapper = new UserEntityMapper();
+        UserProjectListEntityMapper = new UserProjectListEntityMapper();
+        
+        DbContextFactory = new DbContextSqLiteTestingFactory("database");
+        UnitOfWorkFactory = new UnitOfWorkFactory(DbContextFactory);
+    }
+
+    protected IDbContextFactory<ProjectDbContext> DbContextFactory { get; }
+    protected ActivityModelMapper ActivityModelMapper { get; }
+    protected ProjectModelMapper ProjectModelMapper { get; }
+    protected TagModelMapper TagModelMapper { get; }
+    protected TodoModelMapper TodoModelMapper { get; }
+    protected UserModelMapper UserModelMapper { get; }
+
+    protected ActivityEntityMapper ActivityEntityMapper { get; }
+    protected ActivityTagListEntityMapper ActivityTagListEntityMapper { get; }
+    protected ProjectEntityMapper ProjectEntityMapper { get; }
+    protected TagEntityMapper TagEntityMapper { get; }
+    protected TodoEntityMapper TodoEntityMapper { get; }
+    protected UserEntityMapper UserEntityMapper { get; }
+    protected UserProjectListEntityMapper UserProjectListEntityMapper { get; }
+    protected UnitOfWorkFactory UnitOfWorkFactory { get; }
+
+    public async Task InitializeAsync()
+    {
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        await dbx.Database.EnsureDeletedAsync();
+        await dbx.Database.EnsureCreatedAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        await dbx.Database.EnsureDeletedAsync();
+    }
+}
---
+++ project.BL.tests/UserFacadeTests.cs
@@ -0,0 +1,36 @@
+﻿using project.BL.Facades;
+using project.BL.tests.ModelSeeds;
+using Xunit.Abstractions;
+using Xunit.Asserts.Compare;
+
+namespace project.BL.tests;
+
+public class UserFacadeTests : FacadeTestsBase
+{
+    private readonly UserFacade _userFacade;
+    public UserFacadeTests(ITestOutputHelper output) : base(output)
+    {
+        _userFacade = new UserFacade(UnitOfWorkFactory, UserModelMapper);
+    }
+
+    [Fact]
+    public async Task CreateUser_Succeeds()
+    {
+        var userModel = UserSeeds.UserSeed();
+        var idk = await _userFacade.SaveAsync(userModel);
+    }
+
+    [Fact]
+    public async Task CreateUserModel_GetEntity_CompareID()
+    {
+        var userModel = UserSeeds.UserSeed();
+        
+        // Vraci userModel, ale ma novy guid (zmeni se ve fasade)
+        var userModelUpdated = await _userFacade.SaveAsync(userModel);
+    
+        var userFromDb = await _userFacade.GetAsync(userModelUpdated.Id);
+        Assert.NotNull(userFromDb);
+        Assert.Equal(userModelUpdated.Id, userFromDb.Id);
+    }
+    
+}
