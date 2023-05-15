06c1c3968f4e3e1d906b28201546adb7b99d6eb5
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-15 18:19:47 +0200

    feat: Add enumerable to observableCollection extension

---
+++ project.App/EnumerableExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace project.App
+{
+    public static class EnumerableExtension
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> values)
+            => new(values);
+    }
+}
