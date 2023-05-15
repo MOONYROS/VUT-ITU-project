0c39677d828a7ddcf06662820a02925738de4d4f
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-15 22:08:53 +0200

    fix: Update activityview

--- project.App/ViewModels/ActivitiesViewModel.cs
+++ project.App/ViewModels/ActivitiesViewModel.cs
@@ -2,10 +2,27 @@
 
 namespace project.App.ViewModels
 {
-    public class ActivitiesViewModel : ViewModelBase
+    public partial class ActivitiesViewModel : ViewModelBase
     {
-        public ActivitiesViewModel(IMessengerService messengerService) : base(messengerService)
+        public IEnumerable<DateTime> Week { get; set; }
+        public DateTime Today { get; set; }
+
+        public ActivitiesViewModel(IMessengerService messengerService)
+            :base(messengerService)
         {
+            Week = new List<DateTime>();
+            Today = DateTime.Today;
+            while (Today.DayOfWeek != DayOfWeek.Monday)
+            {
+                Today = Today.AddDays(-1);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                Week = Week.Append(Today);
+                Today = Today.AddDays(1);
+            }
         }
+
     }
 }
