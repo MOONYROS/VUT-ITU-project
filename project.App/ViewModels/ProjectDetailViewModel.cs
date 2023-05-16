bf5a8ebb24350e2054a987bf42c110e835b29aca
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-16 23:10:24 +0200

    fix: Delete redundant lines after merge conflict

--- project.App/ViewModels/ProjectDetailViewModel.cs
+++ project.App/ViewModels/ProjectDetailViewModel.cs
@@ -5,17 +5,18 @@ using project.BL.Models;
 
 namespace project.App.ViewModels;
 
-internal class ProjectDetailViewModel : ViewModelBase
+public class ProjectDetailViewModel : ViewModelBase
 {
     private readonly IUserFacade _userFacade;
     private readonly IProjectFacade _projectFacade;
 
-        public ProjectDetailViewModel(IMessengerService messengerService,
-           IUserFacade userFacade,
-           IProjectFacade projectFacade) : base(messengerService)
-        {
-            _userFacade = userFacade;
-            _projectFacade = projectFacade;
-        }
+    public ProjectDetailViewModel(IMessengerService messengerService,
+       IUserFacade userFacade,
+       IProjectFacade projectFacade) : base(messengerService)
+    {
+        _userFacade = userFacade;
+        _projectFacade = projectFacade;
     }
+
+    
 }
