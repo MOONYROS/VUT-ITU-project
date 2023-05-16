52fba05f3342c7664d8d269749933a97a622611f
MOONYROS <ondrej@lukasek.cz>
2023-05-16 21:54:56 +0200

    feat: Empty projects page added

---
+++ project.App/ViewModels/ProjectListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using project.App.Services.Interfaces;
+
+namespace project.App.ViewModels;
+
+public class ProjectListViewModel:ViewModelBase
+{
+    public ProjectListViewModel(IMessengerService messengerService) : base(messengerService)
+    {
+    }
+}
