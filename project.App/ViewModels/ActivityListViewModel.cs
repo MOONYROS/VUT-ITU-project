662da6380c3577975f3a14a04ca90637717333da
Patrik Nehéz <xnehez01@vutbr.cz>
2023-05-16 10:37:01 +0200

    Project actvivity ViewModels

---
+++ project.App/ViewModels/ActivityListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using project.App.Services.Interfaces;
+using project.BL.Facades.Interfaces;
+using project.BL.Models;
+using project.App.Messages;
+
+namespace project.App.ViewModels;
+    public partial class ActivityListViewModel : ViewModelBase
+    {
+        private readonly IActivityFacade _activityFacade;
+        private readonly INavigationService _navigationService;
+        public IEnumerable<ActivityListModel> Activities { get; set; } = null;
+
+        public ActivityListViewModel(IMessengerService messengerService,
+            IActivityFacade activityFacade,
+            INavigationService navigationService) : base(messengerService)
+        {
+            _activityFacade = activityFacade;
+            _navigationService = navigationService;
+        }
+
+        protected override async Task LoadDataAsync()
+        {
+            await base.LoadDataAsync();
+
+            Activities = await _activityFacade.GetAsync();
+        }
+        public async void Receive(ActivityDeleteMessage message)
+        {
+            await LoadDataAsync();
+        }
+
+}
---
+++ project.App/ViewModels/ProjectDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using project.App.Services.Interfaces;
+using project.BL.Facades.Interfaces;
+using project.BL.Models;
+
+namespace project.App.ViewModels
+{
+    internal class ProjectDetailViewModel : ViewModelBase
+    {
+        private readonly IUserFacade _userFacade;
+        private readonly IProjectFacade _projectFacade;
+
+        public ProjectDetailViewModel(IMessengerService messengerService,
+           IUserFacade userFacade,
+           IProjectFacade projectFacade) : base(messengerService)
+        {
+            _userFacade = userFacade;
+            _projectFacade = projectFacade;
+        }
+
+        
+    }
+}
