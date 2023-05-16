f2eabdaf0a260b67ff825eedc603e7eaa6eaaca5
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-16 22:29:38 +0200

    feat: Add todo viewModels

---
+++ project.App/ViewModels/AddTodoViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using project.App.Messages;
+using project.App.Services.Interfaces;
+using project.BL.Facades;
+using project.BL.Facades.Interfaces;
+using project.BL.Models;
+using System.Collections.ObjectModel;
+
+namespace project.App.ViewModels;
+
+[QueryProperty(nameof(UserId), nameof(UserId))]
+public class AddTodoViewModel : ViewModelBase
+{
+    private readonly ITodoFacade _todoFacade;
+    public Guid UserId { get; set; }
+    public ObservableCollection<TodoDetailModel> Todos { get; set; } = new();
+    public TodoDetailModel Todo = TodoDetailModel.Empty;
+    public AddTodoViewModel(IMessengerService messengerService,
+        ITodoFacade todoFacade) : base(messengerService)
+    {
+        _todoFacade = todoFacade;
+    }
+
+    [RelayCommand]
+    public async Task SaveTodoAsync()
+    { 
+        await _todoFacade.SaveAsync(Todo, UserId);
+        messengerService.Send(new TodoAddMessage());
+    }
+
+    protected override async Task LoadDataAsync()
+    {
+        var todos = await _todoFacade.GetAsyncUser(UserId);
+        Todos = todos.ToObservableCollection();
+    }
+
+    public async void Receive(TodoAddMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    public async void Receive(TodoDeleteMessage message)
+    {
+        await LoadDataAsync();
+    }
+}
--- project.App/ViewModels/TodoListViewModel.cs
+++ project.App/ViewModels/TodoListViewModel.cs
@@ -1,10 +1,39 @@
-﻿using project.App.Services.Interfaces;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using project.App.Messages;
+using project.App.Services.Interfaces;
+using project.BL.Facades.Interfaces;
+using project.BL.Models;
+using System.Collections.ObjectModel;
 
 namespace project.App.ViewModels;
 
-public class TodoListViewModel:ViewModelBase
+[QueryProperty(nameof(UserId), nameof(UserId))]
+public class TodoListViewModel : ViewModelBase,
+    IRecipient<TodoAddMessage>,
+    IRecipient<TodoDeleteMessage>
 {
-    public TodoListViewModel(IMessengerService messengerService) : base(messengerService)
+    private readonly ITodoFacade _todoFacade;
+    public Guid UserId { get; set; }
+    public ObservableCollection<TodoDetailModel> Todos { get; set; } = new();
+    public TodoListViewModel(IMessengerService messengerService,
+        ITodoFacade todoFacade) : base(messengerService)
     {
+        _todoFacade = todoFacade;
+    }
+
+    protected override async Task LoadDataAsync()
+    {
+        var todos = await _todoFacade.GetAsyncUser(UserId);
+        Todos = todos.ToObservableCollection();
+    }
+
+    public async void Receive(TodoAddMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    public async void Receive(TodoDeleteMessage message)
+    {
+        await LoadDataAsync();
     }
 }
