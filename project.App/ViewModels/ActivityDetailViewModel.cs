e56d475c6d3d9772a213ebe49d602670100612f4
Pavol Šumichrast <xsumic01@vutbr.cz>
2023-05-17 05:30:11 -0700

    Activity delte

--- project.App/ViewModels/ActivityDetailViewModel.cs
+++ project.App/ViewModels/ActivityDetailViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.Input;
+using project.App.Messages;
 using project.App.Services.Interfaces;
+using project.BL.Facades;
 using project.BL.Facades.Interfaces;
 using project.BL.Models;
 
@@ -9,14 +12,31 @@ namespace project.App.ViewModels;
 public partial class ActivityDetailViewModel : ViewModelBase
 {
     private readonly IActivityFacade _activityFacade;
+    private readonly INavigationService _navigationService;
     public Guid ActivityId { get; set; }
     public Guid UserId { get; set; }
     public ActivityDetailModel Activity { get; set; } = ActivityDetailModel.Empty;
     public ActivityDetailViewModel(
+        INavigationService navigationService,
         IMessengerService messengerService,
         IActivityFacade activityFacade)
         : base(messengerService)
     {
         _activityFacade = activityFacade;
+        _navigationService = navigationService;
+    }
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Activity = await _activityFacade.GetAsync(ActivityId);
+    }
+
+    public async void DeleteActivity(Guid Id)
+    {
+        await _activityFacade.DeleteAsync(Id);
+        messengerService.Send(new ActivityDeleteMessage());
+        await _navigationService.GoToAsync<ActivitiesListViewModel>(
+            new Dictionary<string, object?> { [nameof(ActivitiesListViewModel.UserId)] = UserId });
     }
 }
