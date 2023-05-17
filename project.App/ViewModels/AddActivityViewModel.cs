02a30b7c08c1e4d04b6e8a5f988563702c82eb65
Ondřej Koumar <xkouma02@vutbr.cz>
2023-05-17 11:56:33 +0200

    feat: Add alert service

--- project.App/ViewModels/AddActivityViewModel.cs
+++ project.App/ViewModels/AddActivityViewModel.cs
@@ -4,6 +4,7 @@ using project.App.Services.Interfaces;
 using project.BL.Facades.Interfaces;
 using project.BL.Models;
 using System.Drawing;
+using project.BL;
 
 namespace project.App.ViewModels;
 
@@ -12,30 +13,44 @@ public partial class AddActivityViewModel : ViewModelBase
 {
     private readonly IActivityFacade _activityFacade;
     private readonly INavigationService _navigationService;
+    private readonly IAlertService _alertService;
     public Guid UserId { get; set; }
     public int ColorIndex { get; set; } = 0;
 
     public TimeSpan TimeFrom { get; set; }
     public TimeSpan TimeTo { get; set; }
     public ActivityDetailModel activityDetailModel { get; set; } = ActivityDetailModel.Empty;
-    public AddActivityViewModel(IMessengerService messengerService,
+    public AddActivityViewModel(
+        IMessengerService messengerService,
         IActivityFacade activityFacade,
-        INavigationService navigationService
-        ) : base(messengerService)
+        INavigationService navigationService,
+        IAlertService alertService)
+        : base(messengerService)
     {
         _activityFacade = activityFacade;
         _navigationService = navigationService;
+        _alertService = alertService;
     }
 
     [RelayCommand]
     public async Task SaveActivityAsync()
     {
-        activityDetailModel.DateTimeFrom = activityDetailModel.DateTimeFrom + TimeFrom;
-        activityDetailModel.DateTimeTo = activityDetailModel.DateTimeTo + TimeTo;
-        activityDetailModel.Color = IndexToColor(ColorIndex);
-        await _activityFacade.SaveAsync(activityDetailModel, UserId, null);
-        messengerService.Send(new ActivityAddMessage());
-        activityDetailModel = ActivityDetailModel.Empty;
+        if (TimeFrom < TimeTo && activityDetailModel.Name != string.Empty)
+        {
+            activityDetailModel.DateTimeFrom = activityDetailModel.DateTimeFrom + TimeFrom;
+            activityDetailModel.DateTimeTo = activityDetailModel.DateTimeTo + TimeTo;
+            activityDetailModel.Color = IndexToColor(ColorIndex);
+            try
+            {
+                await _activityFacade.SaveAsync(activityDetailModel, UserId, null);
+            }
+            catch (OverlappingException)
+            {
+                await _alertService.DisplayAsync("Hupsik Dupsik", "Activites are overlapping");
+            }
+            messengerService.Send(new ActivityAddMessage());
+            activityDetailModel = ActivityDetailModel.Empty;
+        }
     }
     private System.Drawing.Color IndexToColor(int index)
     {
--- project.App/ViewModels/AddTodoViewModel.cs
+++ project.App/ViewModels/AddTodoViewModel.cs
@@ -6,6 +6,7 @@ using project.BL.Facades;
 using project.BL.Facades.Interfaces;
 using project.BL.Models;
 using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace project.App.ViewModels;
 
@@ -13,24 +14,42 @@ namespace project.App.ViewModels;
 public partial class AddTodoViewModel : ViewModelBase
 {
     private readonly ITodoFacade _todoFacade;
+    private readonly INavigationService _navigationService;
+    private readonly IAlertService _alertService;
     public Guid UserId { get; set; }
 
     public DateTime Time { get; set; }
     public TodoDetailModel Todo { get; set; } = TodoDetailModel.Empty;
 
-    public AddTodoViewModel(IMessengerService messengerService,
-        ITodoFacade todoFacade) : base(messengerService)
+    public AddTodoViewModel(
+        IMessengerService messengerService,
+        ITodoFacade todoFacade, 
+        INavigationService navigationService,
+        IAlertService alertService) 
+        : base(messengerService)
     {
         _todoFacade = todoFacade;
+        _navigationService = navigationService;
+        _alertService = alertService;
     }
 
     [RelayCommand]
     public async Task SaveTodoAsync()
     {
-        Todo.Finished = false;
-        Todo.Date = DateOnly.FromDateTime(Time);
-        await _todoFacade.SaveAsync(Todo, UserId);
-        messengerService.Send(new TodoAddMessage());
+        if (Todo.Date < DateOnly.FromDateTime(DateTime.Now))
+        {
+            await _alertService.DisplayAsync("Hupsik Dupsik", "too late m8th");
+        }
+        else
+        {
+            Todo.Finished = false;
+            Todo.Date = DateOnly.FromDateTime(Time);
+            await _todoFacade.SaveAsync(Todo, UserId);
+            messengerService.Send(new TodoAddMessage());
+            await _navigationService.GoToAsync<TodoListViewModel>(
+                new Dictionary<string, object?> { [nameof(AddTodoViewModel.UserId)] = UserId });
+        }
+        
     }
 
     public async void Receive(TodoAddMessage message)
