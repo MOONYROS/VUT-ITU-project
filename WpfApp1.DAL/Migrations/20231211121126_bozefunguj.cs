d4ed46c2f62f7c0af88448662afcfc5118c82664
Kumismar <xkouma02@vutbr.cz>
2023-12-11 13:19:36 +0100

    chores: new migration

---
+++ WpfApp1.DAL/Migrations/20231211121126_bozefunguj.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfApp1.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class bozefunguj : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Projects_ProjectId",
+                table: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "UPLists");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Activities_ProjectId",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Activities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectId",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UPLists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UPLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UPLists_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UPLists_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_ProjectId",
+                table: "Activities",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UPLists_ProjectId",
+                table: "UPLists",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UPLists_UserId",
+                table: "UPLists",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Projects_ProjectId",
+                table: "Activities",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
