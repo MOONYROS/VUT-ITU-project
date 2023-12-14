0dc05ee6801aea2c823458c9c05f71f2f91ccb7c
MOONYROS <ondrej@lukasek.cz>
2023-12-14 19:50:30 +0100

    Activity list view

---
+++ WpfApp1.App/Views/ActivityListView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WpfApp1.App.Views;
+
+public partial class ActivityListView : UserControl
+{
+	public ActivityListView()
+	{
+		InitializeComponent();
+	}
+}
