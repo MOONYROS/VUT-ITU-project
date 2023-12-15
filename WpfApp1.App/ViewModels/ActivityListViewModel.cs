b0e64e2de5a24cde65c54fa2937d9e3d957c8ea8
SGMannis <xmalys02@vutbr.cz>
2023-12-15 18:58:09 +0100

    Activity list view progress

--- WpfApp1.App/ViewModels/ActivityListViewModel.cs
+++ WpfApp1.App/ViewModels/ActivityListViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using WpfApp1.App.Messages;
 using WpfApp1.APP.Services.Interfaces;
 using WpfApp1.BL.Facades.Interfaces;
 using WpfApp1.BL.Models;
+using WpfApp1.App;
 
 namespace WpfApp1.APP.ViewModels;
 
@@ -14,27 +18,74 @@ public partial class ActivityListViewModel : ViewModelBase,
 	IRecipient<ActivityAddedMessage>,
 	IRecipient<LogOutMessage>
 {
-	private readonly IActivityFacade _tagActivityFacadeFacade;
+	private readonly IActivityFacade _ActivityFacadeFacade;
+	private readonly ITagFacade _tagFacade;
 	private readonly IMessengerService _messengerService;
 	private readonly INavigationService _navigationService;
 	private ISharedUserIdService _idService;
 	private bool _firstLoad = true;
 
-	public ObservableCollection<ActivityListModel> Tags { get; set; } = new();
+	public DateTime From { get; set; } = DateTime.Now;
+	public DateTime To { get; set; } = DateTime.Now;
+
+	public TagDetailModel SelectedTag { get; set; }
+
+	public ObservableCollection<TagDetailModel> Tags { get; set; } = new();
+	public ObservableCollection<ActivityListModel> Activities { get; set; } = new();
 	
 	public ActivityListViewModel(
 		IMessengerService messengerService,
-		IActivityFacade tagActivityFacadeFacade,
+		IActivityFacade ActivityFacadeFacade,
 		INavigationService navigationService,
-		ISharedUserIdService idService) : base(messengerService)
+		ISharedUserIdService idService, 
+		ITagFacade tagFacade) : base(messengerService)
 	{
 		_messengerService = messengerService;
-		_tagActivityFacadeFacade = tagActivityFacadeFacade;
+		_ActivityFacadeFacade = ActivityFacadeFacade;
 		_navigationService = navigationService;
 		_idService = idService;
+		_tagFacade = tagFacade;
 		_messengerService.Messenger.Register<NavigationMessage>(this);
 		_messengerService.Messenger.Register<ActivityAddedMessage>(this);
 		_messengerService.Messenger.Register<LogOutMessage>(this);
+	}
+
+	protected override async Task LoadDataAsync()
+	{
+		var tmpTags = await _tagFacade.GetAsyncUser(_idService.UserId);
+		Tags = tmpTags.ToObservableCollection();
+		Tags.Insert(0, TagDetailModel.Empty);
+		SelectedTag = Tags.First();
+	}
+	
+	[RelayCommand]
+	private void ApplyFilter()
+	{
+		Guid? TagId;
+		try
+		{
+			if (SelectedTag.Name == String.Empty)
+			{
+				TagId = null;
+			}
+			else
+			{
+				TagId = SelectedTag.Id;
+			}
+		}
+		catch (Exception e)
+		{
+			TagId = null;
+		}
+		if (DateTime.Compare(From, To) < 0)
+		{
+			
+		}
+		else
+		{
+			MessageBox.Show("\"From\" je pozdeji nez \"To\"... Co to zas zkousis...",
+				"Hupsík dupsík...", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
 	}
 	
 	[RelayCommand]
