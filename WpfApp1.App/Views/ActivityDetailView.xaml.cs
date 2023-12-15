e6a292341a47bbc7ff2af3bafeb26fa1342c7e06
MOONYROS <ondrej@lukasek.cz>
2023-12-15 09:02:13 +0100

    Added ActivityDetailView.xaml

---
+++ WpfApp1.App/Views/ActivityDetailView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WpfApp1.App.Views;
+
+public partial class ActivityDetailView : UserControl
+{
+	public ActivityDetailView()
+	{
+		InitializeComponent();
+	}
+}
