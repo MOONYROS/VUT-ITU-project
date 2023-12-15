0a43fef0dc1eb13b54fce4628a178009eeb5288a
Kumismar <xkouma02@vutbr.cz>
2023-12-15 11:20:49 +0100

    feat: color converter
    also fixed namespace in converters

---
+++ WpfApp1.App/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+
+namespace WpfApp1.App.Converters;
+public class ColorToBrushConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		var tmpvalue = (System.Drawing.Color)(value);
+		Color wpfColor = Color.FromArgb(tmpvalue.A, tmpvalue.R, tmpvalue.G, tmpvalue.B);
+		return new SolidColorBrush(wpfColor);
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return null;
+	}
+}
